6cc5ba7cd9b38b2cf5e66c1c3b05ce47c6caf049
mspaireddy <shoba.paireddy1@gmail.com>
2025-08-26 15:42:41 +0100

    Set up working GitHub Actions CI with Jest and TypeScript

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  verbose: true,
-  reporters: [
-    'default',
-    ['jest-html-reporters', {
-      publicPath: './reports',
-      filename: 'report.html',
-      expand: true
-    }]
-  ],
+  testMatch: ['**/tests/**/*.test.ts']
 };
